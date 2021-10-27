70975b77620ad9f987629a7442a8a4cbe65c05e9
Killearnith <diegamer10@gmail.com>
2021-10-27 19:27:58 +0200

    Reloj funciona intermedio, y hora de entrada y salida funciona ya, pero no guarda el estado.

--- app/src/main/java/com/example/proyectosmov/FicharLogic.java
+++ app/src/main/java/com/example/proyectosmov/FicharLogic.java
@@ -2,6 +2,8 @@ package com.example.proyectosmov;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,8 +14,10 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-public class FicharLogic extends AppCompatActivity {
-    TextView textoHora;
+public class FicharLogic extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "FicharLogic";
+    Button entrada;
+    TextView textoHora, hEntrada, hSalida;
     private boolean activo;
     private final Handler mHandler;
     Date hora;
@@ -23,7 +27,7 @@ public class FicharLogic extends AppCompatActivity {
             if (activo) {
                 if (textoHora != null) {
                     //Actualizamos la hora, en funcion del formato, HH:mm:ss, y lo ejecutamos en el manejador con el runnable.
-                    hora=Calendar.getInstance().getTime();
+                    hora = Calendar.getInstance().getTime();
                     String tiempoActual = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(hora);
                     textoHora.setText(tiempoActual);
                 }
@@ -43,12 +47,39 @@ public class FicharLogic extends AppCompatActivity {
         getSupportActionBar().hide();
 
         // Enlazar views
+        entrada = (Button) findViewById(R.id.buttonFichar);
         textoHora = (TextView) findViewById(R.id.actualhora);
+        hEntrada = (TextView) findViewById(R.id.horaentrada);
+        hSalida = (TextView) findViewById(R.id.horasalida);
+        entrada.setOnClickListener(this);
         actualizarHora();
 
     }
-    private void actualizarHora(){
-        activo=true;
+
+    private void actualizarHora() {
+        activo = true;
         mHandler.post(mRunnable);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (entrada.getText().toString().equals("Salida")) {
+            Log.d(TAG, "onClicked: " + entrada.getText().toString());
+            hora = Calendar.getInstance().getTime();
+            String tiempoActual = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(hora);
+            hSalida.setText(tiempoActual);
+            entrada.setText("Entrada");
+
+        }
+        else if ( entrada.getText().toString().equals("Entrada")) {
+            Log.d( TAG,"onClicked: "+entrada.getText().toString());
+            hora = Calendar.getInstance().getTime();
+            String tiempoActual = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(hora);
+            hEntrada.setText(tiempoActual);
+            hSalida.setText(null);
+            entrada.setText("Salida");
+
+        }
     }
 }
