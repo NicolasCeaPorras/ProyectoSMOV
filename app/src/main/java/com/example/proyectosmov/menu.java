6ebae491fad372aea24b386b89c1b132ff6ac848
gackerman54 <gusitavo54@gmail.com>
2021-10-27 09:55:09 +0200

    Incluida en el manifiesto la actividad de Diego y cambios en la interfaz de menu, además de creada la redirección a fichar

--- app/src/main/java/com/example/proyectosmov/menu.java
+++ app/src/main/java/com/example/proyectosmov/menu.java
@@ -2,14 +2,22 @@ package com.example.proyectosmov;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class menu extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         getSupportActionBar().hide();
+
+        LinearLayout fichar = findViewById(R.id.fichar);
+    }
+
+    public void clickFichar (View v){
+        startActivity(new Intent(menu.this,FicharLogic.class));
     }
 }
