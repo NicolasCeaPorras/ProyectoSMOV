1ac002861a20e2fe9912ccff8bb73aa6c6c2b626
Killearnith <diegamer10@gmail.com>
2021-10-27 12:34:44 +0200

    Reloj funciona intermedio.

--- app/src/main/java/com/example/proyectosmov/FicharLogic.java
+++ app/src/main/java/com/example/proyectosmov/FicharLogic.java
@@ -1,6 +1,7 @@
 package com.example.proyectosmov;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,7 +14,28 @@ import java.util.Locale;
 
 public class FicharLogic extends AppCompatActivity {
     TextView textoHora;
-    Date hora=Calendar.getInstance().getTime();
+    private boolean activo;
+    private final Handler mHandler;
+    Date hora;
+    //Referencia usada para comprender el hilo: https://stackoverflow.com/questions/6400846/updating-time-and-date-by-the-second-in-android
+    private final Runnable mRunnable = new Runnable() {
+        public void run() {
+            if (activo) {
+                if (textoHora != null) {
+                    //Actualizamos la hora, en funcion del formato, HH:mm:ss, y lo ejecutamos en el manejador con el runnable.
+                    hora=Calendar.getInstance().getTime();
+                    String tiempoActual = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(hora);
+                    textoHora.setText(tiempoActual);
+                }
+                mHandler.postDelayed(mRunnable, 1000);
+            }
+        }
+    };
+
+    public FicharLogic() {
+        mHandler = new Handler();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +44,11 @@ public class FicharLogic extends AppCompatActivity {
 
         // Enlazar views
         textoHora = (TextView) findViewById(R.id.actualhora);
-        String tiempoActual = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(hora);
-        textoHora.setText(tiempoActual);
+        actualizarHora();
 
     }
-
+    private void actualizarHora(){
+        activo=true;
+        mHandler.post(mRunnable);
+    }
 }
