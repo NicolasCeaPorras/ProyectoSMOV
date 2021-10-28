d810cc5c74aec2777157fa372feb83b9573b7adf
NicolasCeaPorras <nicolasceaporras@gmail.com>
2021-10-28 18:12:55 +0200

    Creada clase para las interacciones con la bd

---
+++ app/src/main/java/com/example/proyectosmov/InteraccionBD.kt
@@ -0,0 +1,22 @@
+package com.example.proyectosmov
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class InteraccionBD {
+
+    val db = Firebase.firestore //Interaccion con la base de datos
+
+    fun leerDatos(coleccion: String, documento: String, campo: String) : String{
+        var consulta = ""
+        db.collection("usuarios").document("email").get().addOnSuccessListener{
+            consulta = it.get(campo) as String
+        }
+        if(consulta.equals("")){
+            return "Error en la lectura"
+        }
+        else{
+            return consulta
+        }
+    }
+}
