3567d48c941d3f5c142a73f20b9d2afb2c5c925a
NicolasCeaPorras <nicolasceaporras@gmail.com>
2021-11-03 19:54:02 +0100

    He empezado el organigrama y va fino

--- app/src/main/java/com/example/proyectosmov/Organigrama.kt
+++ app/src/main/java/com/example/proyectosmov/Organigrama.kt
@@ -2,10 +2,31 @@ package com.example.proyectosmov
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.widget.Button
 
 class Organigrama : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_organigrama)
+        funcion()
+    }
+
+    fun funcion(){
+        val organizationChart= OrganizationChart.getInstance(this)
+        val webView = findViewById<WebView>(R.id.organigrama_webview)
+
+        organizationChart.addChildToParent("Jacob","<div style=\\\"color:red; font-style:italic\\\">President</div>","Mike")
+        organizationChart.addChildToParent("Jacob1","Mike")
+        organizationChart.addChildToParent("Jacob2","Mike")
+        organizationChart.addChildToParent("Jacob3","Mike")
+        organizationChart.addChildToParent("Calson1","Jacob1")
+        organizationChart.addChildToParent("Calson2","Jacob1")
+        organizationChart.addChildToParent("Calson3","Jacob1")
+        organizationChart.addChildToParent("Calson4","Jacob1")
+        organizationChart.addChildToParent("Marcocacho","Calson1")
+        organizationChart.addChildToParent("PutoAmo","Calson1")
+        webView.getSettings().setJavaScriptEnabled(true)
+        webView.loadData(organizationChart.getChart(), "text/html", "UTF-8")
     }
 }
