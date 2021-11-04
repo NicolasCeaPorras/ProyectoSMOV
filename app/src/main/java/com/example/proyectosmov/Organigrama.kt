ee34505dbf3b259168eca96177c4832a00cba04d
NicolasCeaPorras <nicolasceaporras@gmail.com>
2021-11-04 19:42:36 +0100

    Primera version funcional del organigrama

--- app/src/main/java/com/example/proyectosmov/Organigrama.kt
+++ app/src/main/java/com/example/proyectosmov/Organigrama.kt
@@ -3,30 +3,47 @@ package com.example.proyectosmov
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
-import android.widget.Button
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Organigrama : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_organigrama)
-        funcion()
+        supportActionBar!!.hide()
+        logicaOrganigrama()
     }
 
-    fun funcion(){
+    fun logicaOrganigrama(){
         val organizationChart= OrganizationChart.getInstance(this)
         val webView = findViewById<WebView>(R.id.organigrama_webview)
 
-        organizationChart.addChildToParent("Jacob","<div style=\\\"color:red; font-style:italic\\\">President</div>","Mike")
-        organizationChart.addChildToParent("Jacob1","Mike")
-        organizationChart.addChildToParent("Jacob2","Mike")
-        organizationChart.addChildToParent("Jacob3","Mike")
-        organizationChart.addChildToParent("Calson1","Jacob1")
-        organizationChart.addChildToParent("Calson2","Jacob1")
-        organizationChart.addChildToParent("Calson3","Jacob1")
-        organizationChart.addChildToParent("Calson4","Jacob1")
-        organizationChart.addChildToParent("Marcocacho","Calson1")
-        organizationChart.addChildToParent("PutoAmo","Calson1")
+
+        // Llamada a la base de datos
+        val db = FirebaseFirestore.getInstance()
+
+
+        // Lista con todas las jerarquias
+        var lista: MutableList<Array<String>> = mutableListOf(arrayOf("",""))
+        lista.removeAt(0)
+
+        db.collection("usuarios").get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    lista.add(arrayOf(document.get("jefe") as String,document.get("nombre") as String)); // Lista con tupla jerarquia, nombre
+
+                        }
+                for (item in lista) {
+                    organizationChart.addChildToParent(item[1],item[0])
+                    webView.loadData(organizationChart.getChart(), "text/html", "UTF-8")
+                }
+            }
+
+        organizationChart.addChildToParent("","")   // NO QUITAR ESTA LINEA SI LA QUITAS SE ROMPE TODO
+
+
         webView.getSettings().setJavaScriptEnabled(true)
         webView.loadData(organizationChart.getChart(), "text/html", "UTF-8")
     }
 }
+
