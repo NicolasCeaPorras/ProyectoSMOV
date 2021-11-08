6a64419614464c32709d82cdafe4518c6fec33dc
gackerman54 <gusitavo54@gmail.com>
2021-11-08 10:41:52 +0100

    He actualizado el menú y he añadido la funcion (ya funcional)  de añadir y eliminar usuario de la base de datos, que solo sera accesible por los administradores, falta aun control de errores y lograr eliminar un usuario del login propio de firebase

--- app/src/main/java/com/example/proyectosmov/Menu.kt
+++ app/src/main/java/com/example/proyectosmov/Menu.kt
@@ -8,10 +8,12 @@ import android.widget.TextView
 import android.content.Intent
 import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import com.example.proyectosmov.FicharLogic
 import com.example.proyectosmov.LogIn
 import com.example.proyectosmov.AgendaActivity
 import com.example.proyectosmov.VacacionesActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,13 +25,18 @@ class Menu : AppCompatActivity() {
         val email = findViewById<TextView>(R.id.textoEmail)
         val nombre = findViewById<TextView>(R.id.textoNombre)
         val imagenPerfil = findViewById<ImageView>(R.id.imagenPerfil)
+        val gestionUsuarios = findViewById<LinearLayout>(R.id.gestionUsuarios)
+        val lineaGestionUsuarios = findViewById<View>(R.id.lineaGestionUsuarios)
         //Extraigo del bundle el email y lo escribo en el TextView correspondiente
         email.text = bundle!!.getString("email")
         //Buscar el nombre del usuario en la base de datos y escribirlo en el TextView correspondiente
         db.collection("usuarios").document(email.text.toString()).get().addOnSuccessListener {
             nombre.setText("¡Bienvenido, "+it.get("nombre") as String?+"!")
-            if (it.get("administrador") as Boolean)
+            if (it.get("administrador") as Boolean) {
                 imagenPerfil.setImageDrawable(resources.getDrawable(R.drawable.icono_administrador))
+                gestionUsuarios.visibility = View.VISIBLE
+                lineaGestionUsuarios.visibility = View.VISIBLE
+            }
             else
                 imagenPerfil.setImageDrawable(resources.getDrawable(R.drawable.icono_perfil))
         }
@@ -43,8 +50,9 @@ class Menu : AppCompatActivity() {
         startActivity(intent)
     }
 
-    //Evento que al hacer click te cierra la sesion y te redirecciona al login
+    //Evento que al hacer click te cierra la sesion actualmente activa y te redirecciona al login
     fun clickCerrarSesion(v: View?) {
+        FirebaseAuth.getInstance().signOut()
         startActivity(Intent(this@Menu, LogIn::class.java))
     }
     //Evento que al hacer click te lleva a la pantalla de organigrama
@@ -59,5 +67,11 @@ class Menu : AppCompatActivity() {
     //Evento que al hacer click te cierra la sesion y te redirecciona a la pantalla de Vacaciones
     fun clickVacaciones(v: View?) {
         startActivity(Intent(this@Menu, VacacionesActivity::class.java))
+    }
+
+    //Evento que al hacer click te lleva a la pantalla de gestion de usuarios
+    //Unicamente accesible si el usuario que ha iniciado sesion es administrador
+    fun clickGestionUsuarios(v: View?){
+        startActivity(Intent(this@Menu,GestionUsuarios::class.java))
     }
 }
