aa811f5b6de0af2356514fb0cd5bbdce1fe6e14e
NicolasCeaPorras <nicolasceaporras@gmail.com>
2021-11-23 18:43:07 +0100

    Ahora se hace login con compañia+email+password de la base de datos companies

--- app/src/main/java/com/example/proyectosmov/LogIn.kt
+++ app/src/main/java/com/example/proyectosmov/LogIn.kt
@@ -7,8 +7,12 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.proyectosmov.dominio.Company
+import com.example.proyectosmov.dominio.getUserByEmail
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 
 class LogIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,34 +33,53 @@ class LogIn : AppCompatActivity() {
     private fun setup(){
         val botonAcceder = findViewById<Button>(R.id.botonAcceder)
         val email = findViewById<EditText>(R.id.nombreUsuario)
+        val company = findViewById<EditText>(R.id.companiaUsuario)
         val password = findViewById<EditText>(R.id.passwordUsuario)
         val error = findViewById<TextView>(R.id.errorLogin)
 
         // Cuando se pulsa el boton Acceder se valida el usuario en firebase
         botonAcceder.setOnClickListener {
 
-            // Condiciones para que el login pueda ser correcto
-            if (password.text.isNotEmpty() and email.text.isNotEmpty()){
-                if (email.text.contains("@") and email.text.contains(".")) {
-                    error.text = ""
-                    FirebaseAuth.getInstance()
-                        .signInWithEmailAndPassword(email.text.toString(), password.text.toString())
-                        .addOnCompleteListener {
-                            if (it.isSuccessful) {
-                                showMenu(
-                                    it.result?.user?.email ?: ""
-                                )  // Se enseña el menu de la aplicacion y se pasa el mail como parametro
-                            } else {
-                                showAlert() // Muestra el mensaje de error
-                            }
-                        }
+            val db = FirebaseFirestore.getInstance()
+            db.collection("companies").document(company.text.toString()).get().addOnSuccessListener { documentSnapshot ->
+                val company = documentSnapshot.toObject<Company>()
+                if (company == null) {
+                    error.text = "No existe dicha compañia en nuestra base de datos. Por favor vuelva a introducir los datos"
                 }
                 else{
-                    error.text = "No se ha introducido una dirección de mail válida"
+                    val user = getUserByEmail(company, email.text.toString())
+                    if (user == null) {
+                        error.text = "Datos de usuario o contraseña incorrectos. Por favor vuelva a introducir los datos"
+                    }
+                    else{
+
+
+                        // Condiciones para que el login pueda ser correcto
+                        if (password.text.isNotEmpty() and email.text.isNotEmpty()) {
+                            if (email.text.contains("@") and email.text.contains(".")) {
+                                error.text = ""
+                                FirebaseAuth.getInstance()
+                                    .signInWithEmailAndPassword(
+                                        email.text.toString(),
+                                        password.text.toString()
+                                    )
+                                    .addOnCompleteListener {
+                                        if (it.isSuccessful) {
+                                            showMenu(
+                                                it.result?.user?.email ?: ""
+                                            )  // Se enseña el menu de la aplicacion y se pasa el mail como parametro
+                                        } else {
+                                            showAlert() // Muestra el mensaje de error
+                                        }
+                                    }
+                            } else {
+                                error.text = "No se ha introducido una dirección de mail válida"
+                            }
+                        } else {
+                            error.text = "Los campos usuario y contraseña no pueden estar vacios"
+                        }
+                    }
                 }
-            }
-            else{
-                error.text = "Los campos usuario y contraseña no pueden estar vacios"
             }
         }
     }
