efb415d01272856f8d099baf3f0dbea02a00e7f3
Killearnith <diegamer10@gmail.com>
2021-11-25 20:00:51 +0100

    Vacaciones al 21%

--- app/src/main/java/com/example/proyectosmov/VacacionesActivity.kt
+++ app/src/main/java/com/example/proyectosmov/VacacionesActivity.kt
@@ -19,10 +19,9 @@ class VacacionesActivity : AppCompatActivity() {
     var selected_date : Date = Date()
     var selected_item : ScheduledTask? = null
     var Click= 0
-    var diasDisp= 14
+    var diasDisp= 21
     var fechaSal = Date()
     var fechaReg = Date()
-    var diasDispL : Long = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,15 +54,15 @@ class VacacionesActivity : AppCompatActivity() {
 
         calendarView.setOnDateChangeListener(CalendarView.OnDateChangeListener { view, year, month, dayOfMonth ->
             selected_date = SimpleDateFormat("dd-M-yyyy").parse(dayOfMonth.toString() + "-" + (month.toInt() + 1).toString() + "-" + year.toString())
-            Log.i("Vacaciones", "La fecha es: " + selected_date.toString())
             if(Click==1){
                 fechaSalView.setText(selected_date.toString())
                 fechaSal = selected_date
+                Log.i("Vacaciones", "La fecha es: " + selected_date.toString())
 
-            }else if(Click==0) {
+            }else {
                 fechaRegView.setText(selected_date.toString())
                 fechaReg = selected_date
-
+                Log.i("Vacaciones", "La fecha es: " + selected_date.toString())
             }
         })
 
@@ -80,20 +79,26 @@ class VacacionesActivity : AppCompatActivity() {
         val calendarView = findViewById<CalendarView>(R.id.vacaCalendario)
         calendarView.setVisibility(View.VISIBLE)
         Click=0
-
     }
 
     fun onClickGuardar(view: android.view.View) {
         val diasDispView = findViewById<TextView>(R.id.diasVaca)
-
+        var intervalo = 0
         Toast.makeText(this, "¡Fechas Guardadas!", Toast.LENGTH_LONG).show()
+        var cal1 : Calendar = Calendar.getInstance()
+        var cal2 : Calendar = Calendar.getInstance()
+
+        cal1.time = fechaSal
+        cal2.time = fechaReg
+        cal1.get(Calendar.DAY_OF_MONTH)
+        cal2.get(Calendar.DAY_OF_MONTH)
+
+
         if(fechaSal.month==fechaReg.month) {
-            val yearMonthObject: YearMonth = YearMonth.of(fechaSal.year, fechaSal.month)
-            val yearMonth2Object: YearMonth = YearMonth.of(fechaReg.year, fechaReg.month)
-            val dias1: Int = yearMonthObject.lengthOfMonth() //28
-            val dias2: Int = yearMonth2Object.lengthOfMonth() //28
-            diasDisp = (diasDisp - ((fechaReg.day) - (fechaSal.day)))
+
+            diasDisp = (diasDisp - ((cal2.get(Calendar.DAY_OF_MONTH)) - (cal1.get(Calendar.DAY_OF_MONTH))))
         }
+        /*
         else if (fechaSal.month<fechaReg.month) {
             val yearMonthObject: YearMonth = YearMonth.of(fechaSal.year, fechaSal.month)
             val yearMonth2Object: YearMonth = YearMonth.of(fechaReg.year, fechaReg.month)
@@ -102,8 +107,13 @@ class VacacionesActivity : AppCompatActivity() {
 
             diasDisp = (diasDisp - ((dias1+fechaReg.day) - (dias2+fechaSal.day)))
         }
+        */
+
         Log.i("Vacaciones", "Los dias son: " + diasDisp.toString())
-        diasDispView.setText(diasDisp.toString())
+        Log.i("Vacaciones", "Los dias son: " + fechaReg.day.toString())
+        Log.i("Vacaciones", "Los dias son: " + fechaSal.day.toString())
+
+        diasDispView.text = diasDisp.toString()
     }
 
 }
