7ee14bb872c246e5de952db4d36420a3aa2963b1
Killearnith <diegamer10@gmail.com>
2021-12-03 13:13:50 +0100

    Arreglados las condiciones que no se pueden dar a la hora de tomar vacaciones.

--- app/src/main/java/com/example/proyectosmov/VacacionesActivity.kt
+++ app/src/main/java/com/example/proyectosmov/VacacionesActivity.kt
@@ -84,7 +84,7 @@ class VacacionesActivity : AppCompatActivity() {
                     //SI EL USUARIO NO TIENE DIAS ASIGNADOS PORQUE ES ANTIGUO
                     //LE PONEMOS POR DEFECTO 14 (CAMBIAR EN EL FUTURO)
                     if(diasDisp==null) {
-                        //Para testear
+                        //Para testear si el usuario es antiguo
                         diasDisp = 14;
                     }else
                         Log.i("VacacionesAct", "Aqui llego")
@@ -117,56 +117,77 @@ class VacacionesActivity : AppCompatActivity() {
         Toast.makeText(this, "¡Fechas Guardadas!", Toast.LENGTH_SHORT).show()
         val boton = findViewById<Button>(R.id.buttonVacaciones)
 
-        var cal1 : Calendar = Calendar.getInstance()
-        var cal2 : Calendar = Calendar.getInstance()
+        var cal1: Calendar = Calendar.getInstance()
+        var cal2: Calendar = Calendar.getInstance()
 
         cal1.time = fechaSal
         cal2.time = fechaReg
+        if (cal2.get(Calendar.DAY_OF_YEAR) < (cal1.get(Calendar.DAY_OF_YEAR)))
+            Toast.makeText(
+                this,
+                "¡Error, la fecha de llegada no puede ser anterior a la de salida!",
+                Toast.LENGTH_SHORT
+            ).show()
+        else {
+            //Calculo del los dias disponibles que le quedan al usuario de vacaciones una vez ha seleccionado las fechas
+            if (diasDisp < (cal2.get(Calendar.DAY_OF_YEAR)) - (cal1.get(Calendar.DAY_OF_YEAR))) {
+                Toast.makeText(this, "¡Error, has escogido demasiados dias!", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                diasDisp =
+                    (diasDisp - ((cal2.get(Calendar.DAY_OF_YEAR)) - (cal1.get(Calendar.DAY_OF_YEAR))))
 
-        //Calculo del los dias disponibles que le quedan al usuario de vacaciones una vez ha seleccionado las fechas
-        diasDisp = (diasDisp - ((cal2.get(Calendar.DAY_OF_YEAR)) - (cal1.get(Calendar.DAY_OF_YEAR))))
+                Log.i("Vacaciones", "Los dias son: " + diasDisp.toString())
+                Log.i("Vacaciones", "Los dias son: " + fechaReg.day.toString())
+                Log.i("Vacaciones", "Los dias son: " + fechaSal.day.toString())
 
-        Log.i("Vacaciones", "Los dias son: " + diasDisp.toString())
-        Log.i("Vacaciones", "Los dias son: " + fechaReg.day.toString())
-        Log.i("Vacaciones", "Los dias son: " + fechaSal.day.toString())
+                diasDispView.text = diasDisp.toString()
+                boton.isEnabled = false;
+                //Desactivación del boton 2 segundos y activación sucesiva
+                Handler(Looper.getMainLooper()).postDelayed({ boton.isEnabled = true }, 2000)
 
-        diasDispView.text = diasDisp.toString()
-        boton.isEnabled = false;
-        //Desactivación del boton 2 segundos y activación sucesiva
-        Handler(Looper.getMainLooper()).postDelayed({ boton.isEnabled = true }, 2000)
+                val db = FirebaseFirestore.getInstance()
+                val docRef = db.collection("companies").document(companyID)
+                docRef.get().addOnSuccessListener { documentSnapshot ->
+                    val company = documentSnapshot.toObject<Company>()
+                    if (company != null) {
+                        Log.i("VacacionesAct", "Dentro del object Company")
 
-        val db = FirebaseFirestore.getInstance()
-        val docRef = db.collection("companies").document(companyID)
-        docRef.get().addOnSuccessListener { documentSnapshot ->
-            val company = documentSnapshot.toObject<Company>()
-            if (company!=null){
-                Log.i("VacacionesAct", "Dentro del object Company")
+                        val user = getUserByEmail(company, email)
+                        if (user != null) {
+                            var vacPeriodo: HolidayPeriod = HolidayPeriod()
+                            //Damos los valores nuevos
+                            vacPeriodo.creation_date = Date()
+                            vacPeriodo.days_left = diasDisp
+                            vacPeriodo.start_date = fechaSal
+                            vacPeriodo.end_date = fechaReg
 
-                val user = getUserByEmail(company, email)
-                if(user !=null){
-                    var vacPeriodo : HolidayPeriod = HolidayPeriod()
-                    //Damos los valores nuevos
-                    vacPeriodo.creation_date=Date()
-                    vacPeriodo.days_left=diasDisp
-                    vacPeriodo.start_date=fechaSal
-                    vacPeriodo.end_date=fechaReg
+                            //Comprobación de que el la lista contiene o no algo
+                            if (user.holiday_periods == null) {
+                                var lista: MutableList<HolidayPeriod> = mutableListOf()
+                                user.holiday_periods = lista
+                                user.holiday_periods!!.add(vacPeriodo)
+                            } else user.holiday_periods!!.add(vacPeriodo)
 
-                 //Comprobación de que el la lista contiene o no algo
-                 if(user.holiday_periods == null){
-                     var lista: MutableList<HolidayPeriod> = mutableListOf()
-                     user.holiday_periods = lista
-                     user.holiday_periods!!.add(vacPeriodo)
-                 }else user.holiday_periods!!.add(vacPeriodo)
+                            //Actualizamos la BD con los nuevos valores
+                            db.collection("companies").document(companyID)
+                                .update("users", company.users)
+                                .addOnSuccessListener {
+                                    Log.d("VacacionesAct", "Datos actualizados sin problemas")
+                                }
+                                .addOnFailureListener { e ->
+                                    Log.w(
+                                        "VacacionesAct",
+                                        "Fallo en la actualizacion",
+                                        e
+                                    )
+                                }
+                        }
+                    }
 
-                 //Actualizamos la BD con los nuevos valores
-                 db.collection("companies").document(companyID).update("users",company.users).addOnSuccessListener {
-                     Log.d("VacacionesAct", "Datos actualizados sin problemas") }
-                     .addOnFailureListener { e -> Log.w("VacacionesAct", "Fallo en la actualizacion", e) }
                 }
 
             }
-
         }
     }
-
 }
